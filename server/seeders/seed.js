a776e7746b6afbeca29826e0163d86f1a90a2f15
Seannzeribe <snzeribe16@gmail.com>
2023-04-05 22:46:52 -0700

    deleted extra json files and duplicate files

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const db = require('../config/connection');
-const { User, Room } = require('../models');
 const userSeeds = require('./userSeeds.json');
 const roomSeeds = require('./roomSeeds.json');
 
@@ -10,8 +9,7 @@ db.once('open', async () => {
     await User.deleteMany({});
 
     await User.create(userSeeds);
-
-    for (let i = 0; i < roomSeeds.length; i++) {
+    
       const { _id, ownerEmail } = await Room.create(roomSeeds[i]);
       const user = await User.findOneAndUpdate(
         { email: ownerEmail },
